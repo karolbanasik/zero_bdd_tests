b502bb025e891ca753553adc6752edb76c957236
Karol Banasik <banasik.karol@yahoo.com>
2019-01-03 20:16:29 +0100

    feature commit-first feature 27 minutes, this commit 1h 25 minutes. Earlier knowledge of cucumber- 0.01%

---
+++ ZeroBddWebUiTest/Features/ZeroLoginTest.feature
@@ -0,0 +1,14 @@
+﻿Feature: ZeroLoginTest
+	as a User I can login
+
+@Browser: Chrome
+@Browser: Firefox
+@Smoketest
+@Logintest
+
+Scenario: Correct login
+	Given I am at zerobank login page
+	When I enter correct login
+	And I enter correct password
+	And I press Sign in
+	Then I am logged in
---
+++ ZeroBddWebUiTest/Steps/ZeroLoginTestSteps.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace ZeroBddWebUiTest.Steps
+{
+    [Binding]
+    public class ZeroLoginTestSteps
+    {
+        [Given(@"I am at zerobank login page")]
+        public void GivenIAmAtZerobankLoginPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I enter correct login")]
+        public void WhenIEnterCorrectLogin()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I enter correct password")]
+        public void WhenIEnterCorrectPassword()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I press Sign in")]
+        public void WhenIPressSignIn()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+
+        [Then(@"I am logged in")]
+        public void ThenIAmLoggedIn()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
